c30641c3e95ee4a19c4ef49881fe0ae609ca277c
elena-anikina <anikina.e@gmail.com>
2022-01-25 21:20:53 +0100

    App refactoring

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import Header from '../header/header';
 import './app.scss';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/home-page';
 import Layout from '../layout/layout';
 import ArticlePage from '../pages/article-page';
@@ -9,48 +8,43 @@ import SignInPage from '../pages/sign-in-page';
 import SignUpPage from '../pages/sign-up-page';
 import EditProfile from '../pages/edit-profile';
 import ArticleNew from '../pages/article-new';
-import RequireAuthentication from '../../hoc/require-authentication';
+import RequireAuth from '../../hoc/require-authentication';
 import ArticleEdit from '../pages/article-edit';
 
-const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="articles" element={<HomePage />} />
-          <Route path="/articles/:slug" element={<ArticlePage />} />
-          <Route path="/sign-in" element={<SignInPage />} />
-          <Route path="/sign-up" element={<SignUpPage />} />
-          <Route
-            path="/edit-profile"
-            element={
-              <RequireAuthentication>
-                <EditProfile />
-              </RequireAuthentication>
-            }
-          />
-          <Route
-            path="/article-new"
-            element={
-              <RequireAuthentication>
-                <ArticleNew />
-              </RequireAuthentication>
-            }
-          />
-          <Route
-            path="/articles/:slug/edit"
-            element={
-              <RequireAuthentication>
-                <ArticleEdit />
-              </RequireAuthentication>
-            }
-          />
-          <Route path="*" element={<div>No page found</div>} />
-        </Route>
-      </Routes>
-    </>
-  );
-};
+const App = () => (
+  <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<HomePage />} />
+      <Route path="articles" element={<HomePage />} />
+      <Route path="/articles/:slug" element={<ArticlePage />} />
+      <Route path="/sign-in" element={<SignInPage />} />
+      <Route path="/sign-up" element={<SignUpPage />} />
+      <Route
+        path="/edit-profile"
+        element={
+          <RequireAuth>
+            <EditProfile />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/article-new"
+        element={
+          <RequireAuth>
+            <ArticleNew />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/articles/:slug/edit"
+        element={
+          <RequireAuth>
+            <ArticleEdit />
+          </RequireAuth>
+        }
+      />
+    </Route>
+  </Routes>
+);
 
 export default App;
