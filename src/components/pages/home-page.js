a84d58401beb26f60bc636084c2db261ac9cd4cb
elena-anikina <anikina.e@gmail.com>
2022-01-25 18:14:39 +0100

    HomePage refactoring

--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -1,27 +1,26 @@
 import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import List from '../list/list';
-import Pagination from '../pagination/pagination';
 import * as actions from '../../redux/actions';
-import { useLocation } from 'react-router-dom';
 
-const HomePage = ({ fetchArticles, checkingAuthentication, user, articlesCount, pagination: { page } }) => {
-  const location = useLocation();
-  console.log(location.pathname === '/');
-  const numberOfArticlesPerPage = 5;
-  const limit = page * articlesCount;
-  const offset = (page - 1) * numberOfArticlesPerPage;
+const HomePage = ({ fetchArticles, user }) => {
   useEffect(() => {
-    console.log('HomePage UseEffect');
     fetchArticles(5, 0);
-  }, [user]);
-  return (
-    <>
-      <List />
-    </>
-  );
+  }, [user, fetchArticles]);
+
+  return <List />;
 };
 
-const mapStateToProps = (state) => state;
+HomePage.propTypes = {
+  fetchArticles: PropTypes.func.isRequired,
+  user: PropTypes.instanceOf(Object),
+};
+
+HomePage.defaultProps = {
+  user: {},
+};
+
+const mapStateToProps = ({ user }) => ({ user });
 
 export default connect(mapStateToProps, actions)(HomePage);
