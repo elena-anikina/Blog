b0a4b0b25ad07135d3635691ca1f8a90d856c8eb
elena-anikina <anikina.e@gmail.com>
2022-01-25 18:51:30 +0100

    ArticlePage refactoring

--- src/components/pages/article-page.js
+++ src/components/pages/article-page.js
@@ -1,30 +1,32 @@
 import React, { useEffect } from 'react';
-import Article from '../article/article';
+import PropTypes from 'prop-types';
 import { useParams } from 'react-router-dom';
 import { connect } from 'react-redux';
-import RealworldApi from '../../services/realworld-api';
-const realWorldApi = new RealworldApi();
+import Article from '../article/article';
 import * as actions from '../../redux/actions';
 
 const ArticlePage = ({ articles, fetchArticles, getArticleForEditing }) => {
   if (!articles.length) {
-    console.log('Тревога! Тревога! Нет статей!!!');
     fetchArticles();
   }
 
-  useEffect(() => {
-    console.log('fetching article');
-    getArticleForEditing(slug);
-  }, []);
-
   const { slug } = useParams();
+
+  useEffect(() => {
+    getArticleForEditing(slug);
+  }, [getArticleForEditing, slug]);
+
   const [article] = [...articles].filter((el) => el.slug === slug);
 
-  console.log(slug);
-  console.log(article);
   return articles.length ? <Article {...article} preview={false} /> : null;
 };
 
-const mapStateToProps = (state) => state;
+ArticlePage.propTypes = {
+  articles: PropTypes.instanceOf(Array).isRequired,
+  fetchArticles: PropTypes.func.isRequired,
+  getArticleForEditing: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = ({ articles }) => ({ articles });
 
 export default connect(mapStateToProps, actions)(ArticlePage);
